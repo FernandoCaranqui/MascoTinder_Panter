ab1aff8aa7d8da2c2e73fb4b83b26c15bc3920f7
FernandoCaranqui <fernandocaranqui03@gmail.com>
2022-09-02 17:42:27 -0500

    Update Match.java

--- MascoTinder-Pantera/src/main/java/com/modelo/entidades/Match.java
+++ MascoTinder-Pantera/src/main/java/com/modelo/entidades/Match.java
@@ -8,20 +8,65 @@ import javax.persistence.*;
  *
  */
 @Entity
-
+@Table(name = "match")
 public class Match implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Integer id;
-	@Column(name = "Pretendido")
+	@Column(name = "idMascotaPretendido")
 	private Mascota pretendido;
-	@Column(Value = "Pretendiente");
+	@Column(name = "idMascotaPretendiente")
+	private Mascota pretendiente;
 	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Mascota getPretendido() {
+		return pretendido;
+	}
+
+	public void setPretendido(Mascota pretendido) {
+		this.pretendido = pretendido;
+	}
+
+	public Mascota getPretendiente() {
+		return pretendiente;
+	}
+
+	public void setPretendiente(Mascota pretendiente) {
+		this.pretendiente = pretendiente;
+	}
+
 	private static final long serialVersionUID = 1L;
 
 	public Match() {
 		super();
+	}
+	
+	@Override
+	public int hashCode() {
+		int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+	}
+	@Override
+	public boolean equals(Object object) {
+		// TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Match)) {
+            return false;
+        }
+        Match other = (Match) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
 	}
    
 }
