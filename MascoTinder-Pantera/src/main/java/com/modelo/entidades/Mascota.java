c8bc22d81b3ad0dc5b6c3fc495d57d2ba0a47b5c
FernandoCaranqui <fernandocaranqui03@gmail.com>
2022-09-02 16:28:48 -0500

    Creando la clase Mascota

--- MascoTinder-Pantera/src/main/java/com/modelo/entidades/Mascota.java
+++ MascoTinder-Pantera/src/main/java/com/modelo/entidades/Mascota.java
@@ -3,10 +3,14 @@ package com.modelo.entidades;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Mascota")
 public class Mascota  implements Serializable{
 
 	/**
