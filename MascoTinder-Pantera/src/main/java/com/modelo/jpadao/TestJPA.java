6fcacc23e0f4a5a998dce47f1c0a62a53b271e7a
FernandoCaranqui <fernandocaranqui03@gmail.com>
2022-09-03 01:58:22 -0500

    Ingresando Mascotas

--- MascoTinder-Pantera/src/main/java/com/modelo/jpadao/TestJPA.java
+++ MascoTinder-Pantera/src/main/java/com/modelo/jpadao/TestJPA.java
@@ -1,5 +1,7 @@
 package com.modelo.jpadao;
 
+import java.util.List;
+
 import com.modelo.dao.DAOFactory;
 import com.modelo.entidades.Duenio;
 import com.modelo.entidades.Mascota;
@@ -7,13 +9,32 @@ import com.modelo.entidades.Mascota;
 public class TestJPA {
 
 	public static void main(String[] args) {
-			Duenio r = new Duenio();
-			r.setNombre("Kevin");
-			r.setPassword("123456");
-			r.setUser("kevin123");
-			r.setMascotas(null);
+		Duenio d = new Duenio();
+		d.setId(2);
+		/*d.setNombre("Fernando");
+		d.setUser("Fer123");
+		d.setPassword("1234");
+		DAOFactory.getFactory().getDuenioDAO().create(d);
+		*/
+		Mascota r = new Mascota();
+		r.setNombre("Cooper");
+		r.setEdad(1);
+		r.setSexo("Masculino");
+		r.setUbicacion("San Juan");
+		r.setEspecie("Perro");
+		r.setRaza("Mestiza");
+		r.setDuenio(d);
+		/*r.setPreferencias(null);
+		r.setMatch(null);
+		r.setMatches(null);
+		r.setFotosMasc(0);*/
+		
+		DAOFactory.getFactory().getMascotaDAO().create(r);
+
+		
 			
-			DAOFactory.getFactory().getDuenioDAO().create(r);
 	}
+	
+	
 
 }
