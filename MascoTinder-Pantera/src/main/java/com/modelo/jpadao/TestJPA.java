191b374a70114df36e5e4f9b47c1852ba42df7e1
Cristopher0206 <cristopher.perez@epn.edu.ec>
2022-09-02 23:39:43 -0500

    ...

--- MascoTinder-Pantera/src/main/java/com/modelo/jpadao/TestJPA.java
+++ MascoTinder-Pantera/src/main/java/com/modelo/jpadao/TestJPA.java
@@ -8,9 +8,9 @@ public class TestJPA {
 
 	public static void main(String[] args) {
 			Duenio r = new Duenio();
-			r.setNombre("Kevin");
-			r.setPassword("123456");
-			r.setUser("kevin123");
+			r.setNombre("Cristopher");
+			r.setPassword("1234567");
+			r.setUser("cris123");
 			r.setMascotas(null);
 			
 			DAOFactory.getFactory().getDuenioDAO().create(r);
